0dbb0a7186cd2e4f510b0b3cc88e5dee7757bf3c
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2018-11-18 20:27:29 -0800

    Finish Ch2

---
+++ ch2/guessing_game/src/main.rs
@@ -0,0 +1,46 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    
+    println!("Your number is: {}\n", secret_number);
+    
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // With explicit type annotation
+        // let guess: u32 = match guess.trim().parse() {
+        //     Ok(num) => num,
+        //     Err(_) => continue,
+        // };
+
+        // With turbofish
+        // reference: https://doc.rust-lang.org/std/primitive.str.html#method.parse
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}

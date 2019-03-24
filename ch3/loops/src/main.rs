5119294957bc456463b7f8463c3233c8d67f28a3
Don Robert Pornaras <donrobert.pornaras@gmail.com>
2019-03-24 15:58:52 -0700

    Add Ch3 loops

---
+++ ch3/loops/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    // Loop with conditional
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    // While loop
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value at index {} is: {}", index, a[index]);
+
+        index = index + 1;
+    }
+
+    // For loop -- slightly faster for iterating over collections, since 
+    // compiler does not need to check every iteration if out of bounds
+    // at runtime.
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
